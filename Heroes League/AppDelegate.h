4a427145fad50d0cc2f25c93ae12fd8e5825cf83
Ilhom Khodjaev <thenexus85@gmail.com>
2016-07-02 17:50:16 +0500

    Initial Commit

---
+++ Heroes League/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Heroes League
+//
+//  Created by Ilkhom Khodjaev on 7/2/16.
+//  Copyright © 2016 HL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Heroes League/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Heroes League
+//
+//  Created by Ilkhom Khodjaev on 7/2/16.
+//  Copyright © 2016 HL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
